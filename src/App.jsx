0fda4d5a9d4f829a501f5a6fb5822f0d3869b8d6
RoneshSingh <78497324+RoneshSingh@users.noreply.github.com>
2023-07-10 15:33:32 -0400

    Initialize todo app

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+import "./assets/app.css";
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+  const [taskInput, setTaskInput] = useState("");
+
+  const handleChange = (event) => {
+    setTaskInput(event.target.value);
+  };
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos/")
+      .then((response) => response.json())
+      .then((data) => {
+        setTasks(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching tasks:", error);
+      });
+  }, []);
+
+  const addTask = () => {
+    const newTask = {
+      id: tasks.length + 1,
+      title: taskInput,
+      completed: false,
+    };
+
+    setTasks((prevTasks) => [newTask, ...prevTasks]);
+  };
+
+  return (
+    <div>
+      <h1>Todo App</h1>
+      <input type="text" value={taskInput} onChange={handleChange}></input>
+      <button onClick={() => addTask()}>Add Task</button>
+      <ul>
+        {tasks.map((task) => (
+          <li className="beauti-list" key={task.id}>
+            {task.title}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
